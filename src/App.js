4f5c494551f3bc8aad2f65cf9869dc48bc4429e2
= <=>
2024-10-30 18:37:55 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DadosApi from './components/DadosApi';
+
+function App() {
+  return (
+    <div className="App">
+      <DadosApi />
+    </div>
+  );
+}
+
+export default App;
